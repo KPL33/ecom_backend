304068df7a9670a3829bf605f0e6e79f5ff3f3b9
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2023-07-04 16:41:21 -0700

    additional info added to product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,18 +4,23 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', async (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
-  try {
-    const productData = await Product.findAll({
-      include: [Category]
+router.get('/', (req, res) => {
+  Product.findAll({
+    include: [
+      Category,
+      {
+        model: Tag,
+        through: ProductTag,
+      },
+    ],
+  })
+    .then((products) => res.json(products))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
-    res.status(200).json(productData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
 });
+
 
 // get one product
 router.get('/:id', async (req, res) => {
