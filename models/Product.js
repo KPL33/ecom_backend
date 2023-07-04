4e3b54f5915a673c4de11012e13cf53f52d89d81
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2023-07-03 17:59:47 -0700

    models filled out

--- models/Product.js
+++ models/Product.js
@@ -6,9 +6,10 @@ const sequelize = require('../config/connection');
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
-// set up fields and rules for Product model
+//Here, we have set up the fields and rules for the 'Product' 'Model'.
 Product.init(
   { 
+    //Each of the following 'objects' can be considered columns in our overall table and each has its own set of rules for the data it stores.
     id: { 
       type: DataTypes.INTEGER,
       allowNull: false,
@@ -19,19 +20,22 @@ Product.init(
       type: DataTypes.STRING,
       allowNull: false
     },
+    //Here, we do want the 'price' to be an 'INTEGER', but we also want control over both its "math base" (10, in our case) and the number of decimals in the price (2).
     price: {
       type: DataTypes.DECIMAL(10,2),
       allowNull: false,
+      //Here, we allow only 'price' 'value's in that adhere to our "math base" and 'DECIMAL' rules established above.
       validate: {
         isDecimal: true
       }
     },
+    //Similar to 'price' we 'validte' that the 'value' 'isNumberic' and give the stored 'stock' data a 'defaultValue'.
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
       defaultValue: 10,
       validate: {
-        isNumberic: true
+        isNumeric: true
       }
     },
     category_id: {
--- models/index.js
+++ models/index.js
@@ -1,3 +1,11 @@
+//Note: Dane said we don't need to do anything with schema.sql file.
+
+//Already did 'npm i': mysql2, sequelize, express, dotenv
+
+//Check: Do I need to 'require' those in this index.js (or the other one, or neither)?
+
+//Don't forget... we are using SEQUELIZE, not just sql.
+
 // import models
 const Product = require('./Product');
 const Category = require('./Category');
@@ -5,26 +13,30 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+//Each product has 1 category
 Product.belongsTo(Category, {
   foreignKey: 'category_id'
-})
+});
 
 // Categories have many Products
+//1 category has many products
 Category.hasMany(Product, {
   foreignKey: 'category_id'
-})
+});
+
+//Note: It is possible that the foreignKey of 1 of the above 2 is incorrect (prob for 'Category')
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsTo(Tag, {
   through: ProductTag,
   foreignKey: 'product_id'
-})
+});
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsTo(Product, {
   through: ProductTag,
   foreignKey: 'product_id'
-})
+});
 
 module.exports = {
   Product,
